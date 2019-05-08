67698a66e92958eb057399f2c9ee7199b97cf055
ChungTingHao <jason.chung.0211@gmail.com>
2019-05-08 18:20:51 +0800

    test1

---
+++ GuessNumber.php
@@ -0,0 +1,23 @@
+<?php
+  include 'util.php';
+  $result = $hist= '';
+  if(!isset($_POST['guess'])){
+    $answer = creatAnswer(3);
+    // echo $answer;
+  }else {
+    $guess = $_POST['guess'];
+    $answer = $_POST['answer'];
+    $hist = $_POST['hist'];
+    $result = checkAB($guess,$answer);
+    $hist .= "$guess => $result<br>";
+  }
+ ?>
+<form method="post">
+  Guess Number: <input type="number" name="guess" value="<?php echo $guess; ?>">
+  <input type="hidden" name="answer" value="<?php echo $answer; ?>">
+  <input type="hidden" name="hist" value="<?php echo $hist; ?>">
+  <input type="submit" value="Guess">
+  <?php echo $result; ?>
+</form>
+<hr>
+<?php echo $hist ?>
